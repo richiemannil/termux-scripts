5053ab90671830bbed9f30a40bb04b10aa4f28e7
Leo <richardxmannil@gmail.com>
2026-03-16 07:21:21 +0400

    update 2026-03-16

--- syscheck.sh
+++ syscheck.sh
@@ -1,11 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
-echo "--- [ System Status ] ---"
-fastfetch
-echo ""
-echo "--- [ Syncing Time ] ---"
+echo "--- [ Syncing Time via Google ] ---"
+# Improved parsing to avoid "Max 1 argument" error
+GDATE=$(curl -sI google.com | grep -i '^date:' | cut -d' ' -f3-6)
+su -c "date -s \"$GDATE\""
 su -c "settings put global auto_time 0 && settings put global auto_time 1"
-date
+echo "Current Time: $(date)"
 echo ""
-echo "--- [ Checking for OTA Updates ] ---"
-su -c "ls /data/evolution_updates || echo 'No updates found in default folder.'"
-echo "------------------------"
+echo "--- [ Checking OTA ] ---"
+su -c "ls /data/evolution_updates || echo 'No updates found.'"
